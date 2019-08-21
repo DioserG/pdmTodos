7c47db66690fb217e906db943ba8624566397853
DioserG <54085379+DioserG@users.noreply.github.com>
2019-08-21 16:36:03 -0300

    imagem3

--- app/src/main/java/com/m79196/pdmaula3/Main5Activity.java
+++ app/src/main/java/com/m79196/pdmaula3/Main5Activity.java
@@ -1,6 +1,8 @@
 package com.m79196.pdmaula3;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +13,13 @@ public class Main5Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main5);
+
+
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.quiz_transito_icon);
+        getSupportActionBar().setBackgroundDrawable(
+                new ColorDrawable(Color.parseColor("#000000")));
     }
 
     public void btnVoltaMain2(View view) {
