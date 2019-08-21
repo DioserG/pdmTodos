19585e75745f4c4d2f23c472590709b260e12626
m79196 <dioser13@gmail.com>
2019-08-20 21:41:26 -0300

    primeira imagem

--- app/src/main/java/com/m79196/pdmaula3/Main3Activity.java
+++ app/src/main/java/com/m79196/pdmaula3/Main3Activity.java
@@ -1,6 +1,8 @@
 package com.m79196.pdmaula3;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +13,12 @@ public class Main3Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.poupanca_icon);
+        getSupportActionBar().setBackgroundDrawable(
+                new ColorDrawable(Color.parseColor("#000000")));
     }
 
     public void btnVoltaMain2(View view) {
@@ -18,5 +26,7 @@ public class Main3Activity extends AppCompatActivity {
         Intent intent  = new Intent(Main3Activity.this,
                 Main2Activity.class);
         startActivity(intent);
+
+
     }
 }
