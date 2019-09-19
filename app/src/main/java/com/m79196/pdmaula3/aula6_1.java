5fe871914a923c1c91218737a378c3cf36840312
DioserG <54085379+DioserG@users.noreply.github.com>
2019-09-19 17:33:51 -0300

    menu

--- app/src/main/java/com/m79196/pdmaula3/aula6_1.java
+++ app/src/main/java/com/m79196/pdmaula3/aula6_1.java
@@ -1,15 +1,20 @@
 package com.m79196.pdmaula3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.Arrays;
 
 public class aula6_1 extends AppCompatActivity {
 
     private ListView meuListView;
+    String [] listas = {"Dioser","Guilherme","Anderson","Felipe","Leonardo","Sabrina","Rodrigo","Sergio","Sirio","Luiz"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +22,30 @@ public class aula6_1 extends AppCompatActivity {
         setContentView(R.layout.activity_aula6_1);
 
         meuListView = findViewById(R.id.meuListView);
-
-        String [] listas = {"Dioser","Guilherme","Anderson","Felipe","Leonardo","Sabrina","Rodrigo","Sergio","Sirio","Luiz"};
-
-        ArrayAdapter<String> adaptList = new ArrayAdapter(this, android.R.layout.simple_list_item_1, Arrays.asList(listas));
+        final ArrayAdapter<String> adaptList = new ArrayAdapter(this, android.R.layout.simple_list_item_1, Arrays.asList(listas));
 
         meuListView.setAdapter(adaptList);
+
+        /* Selecionar item da lista */
+        meuListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+
+               //String item = listas[position];
+                // Toast.makeText(getApplicationContext(), item, Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(getApplicationContext(), /* activity a ser chamada */ aula6_2.class);
+                String item = (String) listas[position];
+                intent.putExtra("nome", item);
+                // Toast.makeText(getApplicationContext(), item, Toast.LENGTH_SHORT).show();
+                intent.putExtra("POSITION", position);
+                startActivity(intent);
+
+
+            }
+        });
+
     }
+
+
 }
---
+++ app/src/main/java/com/m79196/pdmaula3/aula6_2.java
@@ -0,0 +1,51 @@
+package com.m79196.pdmaula3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
+import android.widget.TextView;
+
+import java.util.Arrays;
+
+public class aula6_2 extends AppCompatActivity {
+
+    private Spinner spinner_aula6_3;
+    private TextView texto1;
+
+    String [] estados = {"RS","SC"};
+    String [] cidade = {"Santa Cruz","Camburiú"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aula6_2);
+
+        spinner_aula6_3 = findViewById(R.id.spinner_estado_aula6_3);
+
+        ArrayAdapter<String> adaptSpinnerEstado = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1, estados);
+        
+        spinner_aula6_3.setAdapter(adaptSpinnerEstado);
+
+        spinner_aula6_3.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
+        ArrayAdapter<String> adaptList = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1, Arrays.asList(cidade));
+
+
+    }
+
+
+
+}
