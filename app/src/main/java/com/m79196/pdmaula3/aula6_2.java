051824c718350784d3443a2d4d6f899d5e76bad3
DioserG <dioser13@gmail.com>
2019-09-20 09:59:12 -0300

    Final desafio implementado

--- app/src/main/java/com/m79196/pdmaula3/aula6_2.java
+++ app/src/main/java/com/m79196/pdmaula3/aula6_2.java
@@ -14,43 +14,56 @@ import java.util.Arrays;
 
 public class aula6_2 extends AppCompatActivity {
 
-    private Spinner spinner_aula6_3;
+    private Spinner spinner_estado;
     private TextView nomeSelecionado;
+    private ListView lv_cidade;
 
-    String [] estados = {"RS","SC"};
-    String [] cidade = {"Santa Cruz","Camburiú"};
+    String [] estado = {"RS","SC","PR"};
+    String [] cidadeRS = {"Santa Cruz","Venãncio Aires","Santa Maria"};
+    String [] cidadeSC = {"Criciúma","Chapecó","Blumenau"};
+    String [] cidadePR = {"Londrina","Pinhais","Curitiba"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aula6_2);
 
-
-        spinner_aula6_3 = findViewById(R.id.spinner_estado_aula6_3);
-
+        // Trazendo valor para o textView
         Bundle extras = getIntent().getExtras();
-        nomeSelecionado = findViewById(R.id.textView_aula6_3);
+        nomeSelecionado = findViewById(R.id.textView_aula6_2);
         nomeSelecionado.setText((String)extras.get("selecionado"));
 
-        ArrayAdapter<String> adaptSpinnerEstado = new ArrayAdapter(this,
-                android.R.layout.simple_list_item_1, estados);
-        
-        spinner_aula6_3.setAdapter(adaptSpinnerEstado);
+        // Colocando valor no Spinner
+        spinner_estado = findViewById(R.id.sp_aula6_2_estado);
 
-        spinner_aula6_3.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        ArrayAdapter<String> adaptSpinnerEstado = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1, estado);
+
+        spinner_estado.setAdapter(adaptSpinnerEstado);
+        spinner_estado.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+                String estadosSelecionados = estado[position];
+
+                // Colocando valor na listView
+                lv_cidade = findViewById(R.id.lv_aula6_2_cidade);
+                if(estadosSelecionados == "RS"){
+                    ArrayAdapter<String> adaptListCidade = new ArrayAdapter(getApplicationContext(),
+                            android.R.layout.simple_list_item_1, Arrays.asList(cidadeRS));
+                            lv_cidade.setAdapter(adaptListCidade);
+                }else if (estadosSelecionados == "SC"){
+                    ArrayAdapter<String> adaptListCidade = new ArrayAdapter(getApplicationContext(),
+                            android.R.layout.simple_list_item_1, Arrays.asList(cidadeSC));
+                            lv_cidade.setAdapter(adaptListCidade);
+                }else if (estadosSelecionados == "PR"){
+                    ArrayAdapter<String> adaptListCidade = new ArrayAdapter(getApplicationContext(),
+                            android.R.layout.simple_list_item_1, Arrays.asList(cidadePR));
+                            lv_cidade.setAdapter(adaptListCidade);
+                }
             }
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
             }
         });
-        ArrayAdapter<String> adaptList = new ArrayAdapter(this,
-                android.R.layout.simple_list_item_1, Arrays.asList(cidade));
-
-
     }
-
-
-
 }
