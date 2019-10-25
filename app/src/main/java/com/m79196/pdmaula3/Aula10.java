0bfed837b1d917558f0c56c3e4902371d7197dfd
DioserG <54085379+DioserG@users.noreply.github.com>
2019-10-25 16:54:59 -0300

    aula10

--- app/src/main/java/com/m79196/pdmaula3/Aula10.java
+++ app/src/main/java/com/m79196/pdmaula3/Aula10.java
@@ -1,13 +1,25 @@
 package com.m79196.pdmaula3;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.loopj.android.http.*;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
+
+import java.security.spec.ECField;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -33,8 +45,40 @@ public class Aula10 extends AppCompatActivity {
         adapter = new MeuAdpter(getApplicationContext(), lista, R.layout.linha_clima, de, para);
         listView.setAdapter(adapter);
 
+      /*  JSONObject res = new JSONObject(result);
+        JSONArray array = res.getJSONArray("weather");
+        for (int i = 0; i < array.length();i++){
+            JSONObject json = array.getJSONObject(i);
+            String temperatura = json.get("temperatura").toString();
+            String humidade = json.get("humidade").toString();
+            String pressao = json.get("pressao_atm").toString();
+            String data_hora = json.get("data_hora").toString();
+        }*/
+
+
         // código a seguir rereferente ao loopj
         String url = "http://ghelfer.net/la/weather.json";
+        RequestParams params = new RequestParams();
+        params.add("weather","1");
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.post(url, params, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] response) {
+                try {
+                    String data = new String(response, "UTF-8");
+                   // res.setText(data);
+
+                }catch (Exception ex){
+                    Log.e("URL", ex.getMessage());
+                };
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                    Log.e("URL", error.toString());
+            }
+        });
 
 
 
